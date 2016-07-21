849f797b4c11cd07f2a9b994de589e737dc194c1
emam <eMaM151987@gmail.com>
2016-07-21 12:03:39 +0200

    settings

--- gowWebsite/settings.py
+++ gowWebsite/settings.py
@@ -163,11 +163,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT ='/home/eMaM1921990/static'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
 
 MEDIA_BASE='/home/emam/'
 MEDIA_ROOT = os.path.join(MEDIA_BASE, 'media')
