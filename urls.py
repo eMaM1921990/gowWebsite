013e26f1930fad2a80e02b5de6bbca7aa16da5ee
ahmed_elemam <eMaM151987@gmail.com>
2016-07-22 23:26:39 +0200

    requirement

---
+++ urls.py
@@ -0,0 +1,36 @@
+"""gowWebsite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include, patterns
+from django.contrib import admin
+from gate_of_world_app import views
+from gowWebsite import settings
+
+urlpatterns = [
+
+    url(r'^grappelli/', include('grappelli.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home),
+    url(r'^sections/(?P<pk>[0-9]+)/$',views.categoryNews, name='categoryNews'),
+    url(r'^lastnews/',views.latestNews, name='lastnews'),
+    url(r'^details/(?P<pk>[0-9]+)/$',views.details, name='news'),
+]
+if settings.DEBUG:
+    # static files (images, css, javascript, etc.)
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT}))
+
+
