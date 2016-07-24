a73e92124fd5ff89b6a221cc10a9435a8f683eeb
ahmed_elemam <eMaM151987@gmail.com>
2016-07-24 22:59:19 +0200

    settings

--- gowWebsite/settings.py
+++ gowWebsite/settings.py
@@ -107,9 +107,9 @@ WSGI_APPLICATION = 'gowWebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 #Database Info
-DB_USER='root'
-DB_PASSWORD='admin'
-DB_NAME='gate_of_world'
+DB_USER='gate_user_'
+DB_PASSWORD='Azsxdcfv@'
+DB_NAME='gate_'
 DB_HOST='localhost'
 DB_PORT=3306
 
