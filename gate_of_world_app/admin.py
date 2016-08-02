2715e57b49dd335912647ae57c4e0db05fda3940
emam <eMaM151987@gmail.com>
2016-08-02 17:54:25 +0200

    video page

--- gate_of_world_app/admin.py
+++ gate_of_world_app/admin.py
@@ -32,7 +32,7 @@ class RSSProviders(admin.ModelAdmin):
 #RSS FEED
 class Feed(admin.ModelAdmin):
     fields = ('thumbnail','feed_title','rss_link','rss_description','rss_category','rss_views_no','rss_video','rss_full_article',)
-    list_display = ('thumbnail','feed_title','rss_publish_date','rss_category')
+    list_display = ('thumbnail','feed_title','rss_publish_date','rss_category','rss_views_no','rss_video',)
     list_per_page = 10
     list_filter = ( 'rss_category',)
     search_fields = ('id', 'rss_link', 'rss_title','rss_publish_date')
