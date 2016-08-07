d0fee53c62109e008397d4dcd89672dbb04f50cd
emam <eMaM151987@gmail.com>
2016-08-07 13:06:28 +0200

    mail

--- gate_of_world_app/forms.py
+++ gate_of_world_app/forms.py
@@ -17,13 +17,13 @@ class ContactForm(forms.Form):
     ]
 
     first_name = forms.CharField(max_length=30, label='First name',
-                                 widget=forms.TextInput(attrs={'placeholder': 'First name','class':'form-control input-md'}), required=True)
+                                 widget=forms.TextInput(attrs={'placeholder': 'اﻷسم اﻷول','class':'form-control input-md'}), required=True)
     last_name = forms.CharField(max_length=30, label='Last name',
-                                widget=forms.TextInput(attrs={'placeholder': 'Last name','class':'form-control input-md'}), required=True)
+                                widget=forms.TextInput(attrs={'placeholder': 'اﻷسم اﻷخير','class':'form-control input-md'}), required=True)
     email = forms.EmailField(max_length=30, label='Email', widget=forms.TextInput(attrs={'placeholder': 'Email','class':'form-control input-md'}),
                              required=True)
     department = forms.ChoiceField(choices=typeChoices, label='Department', widget=forms.Select(attrs={'class':'form-control'}), required=True)
-    content = forms.CharField(required=True, widget=forms.Textarea(attrs={'class':'form-control input-md'}))
+    content = forms.CharField(required=True, widget=forms.Textarea(attrs={'class':'form-control input-md','placeholder':'المحتوى'}))
 
     def is_valid(self):
         if hasattr(self, 'cleaned_data'):
