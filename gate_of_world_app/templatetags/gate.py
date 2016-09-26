9b433c2e4348e7760d09c9e076c891784148da6e
ahmed_elemam <eMaM151987@gmail.com>
2016-09-27 00:32:39 +0200

    templatetags check img url

---
+++ gate_of_world_app/templatetags/gate.py
@@ -0,0 +1,13 @@
+import urllib2
+
+__author__ = 'emam'
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def get_or_404(url):
+    ret = urllib2.urlopen(url)
+    if ret.code == 200:
+        return True
+    return False
