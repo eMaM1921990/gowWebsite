421e4a2eda21c4ccdb39865ad7e70fa858b08350
ahmed_elemam <eMaM151987@gmail.com>
2016-09-28 01:00:04 +0200

    remove exclude from common world

--- gate_of_world_app/Feed.py
+++ gate_of_world_app/Feed.py
@@ -209,7 +209,7 @@ class Feed():
     def findImageIfExist(self, fullArticle):
         soup = BeautifulSoup(fullArticle,"html.parser")
         links = soup.find_all('img', src=True)
-        if len(links > 0):
+        if len(links) > 0:
             return links[0]['src']
         return None
 
