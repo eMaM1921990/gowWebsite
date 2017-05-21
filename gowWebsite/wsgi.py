a8cef52f597f18e04f0aceeada17f1a0210ac06f
Ahmed Elemam <emam151987@gmail.com>
2017-05-21 19:37:33 +0200

    Godday Cloud conf

--- gowWebsite/wsgi.py
+++ gowWebsite/wsgi.py
@@ -6,11 +6,23 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
+#
+# import os
+#
+# from django.core.wsgi import get_wsgi_application
+#
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gowWebsite.settings")
+#
+# application = get_wsgi_application()
+
 
 import os
+import sys
+
+sys.path.append('/opt/bitnami/apps/django/django_projects/gowWebsite')
+os.environ['PYTHON_EGG_CACHE']="/opt/bitnami/apps/django/django_projects/gowWebsite/egg_cache"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gowWebsite.settings.base")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gowWebsite.settings")
 
 application = get_wsgi_application()
